1f5ebcd2499c37fb661449c5baec756ec7057e84
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2022-05-18 11:51:43 -0700

    Updated 404.js

--- 404.js
+++ 404.js
@@ -1,3 +1,21 @@
 var currentUrl = location.href;
 let pageTitle = document.querySelector('.current-URL-not-found')
 pageTitle.innerHTML = '<a href="' + currentUrl + '">' + currentUrl + '</a> Was Not Found'
+
+var splitUrl = currentUrl.split('rockgamerak.com/')[1]
+
+if (splitUrl == '404') {
+  
+}
+else if (splitUrl == 'about/') {
+  location.href = '/about'
+}
+else if (splitUrl == 'contact/') {
+    location.href = '/contact'
+}
+else if (splitUrl == 'projects/') {
+    location.href = '/projects'
+}
+else if (splitUrl == 'sites/') {
+    location.href = '/sites'
+}
