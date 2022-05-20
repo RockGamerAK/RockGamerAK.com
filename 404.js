7f61a6e82994eba8cc30f386dd316d227b69afc6
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2022-05-19 18:59:53 -0700

    Update 404.js

--- 404.js
+++ 404.js
@@ -4,18 +4,24 @@ pageTitle.innerHTML = '<a href="' + currentUrl + '">' + currentUrl + '</a> Was N
 
 var splitUrl = currentUrl.split('://rockgamerak.com/')[1]
 
-if (splitUrl == '404/') {
+if (splitUrl === '404/') {
     location.href = '/404'  
 }
-else if (splitUrl == 'about/') {
+else if (splitUrl === 'about/') {
   location.href = '/about'
 }
-else if (splitUrl == 'contact/') {
+else if (splitUrl === 'contact/') {
     location.href = '/contact'
 }
-else if (splitUrl == 'projects/') {
+else if (splitUrl === 'projects/') {
     location.href = '/projects'
 }
-else if (splitUrl == 'sites/') {
+else if (splitUrl === 'sites/') {
     location.href = '/sites'
+}
+else  if (splitUrl == '__repo') {
+    location.href = 'https://github.com/RockGamerAK/RockGamerAK.com'
+}
+else  if (splitUrl == '__dev') {
+    location.href = 'https://github.dev/RockGamerAK/RockGamerAK.com'
 }
